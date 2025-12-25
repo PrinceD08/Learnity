e8ddefe47a63cd3b220ab95a9b7628b5542c0985
PrinceD08 <princedavid2001101@gmail.com>
2025-12-25 21:19:10 +0100

    Fixed Issues

--- Signup/signup.js
+++ Signup/signup.js
@@ -2,7 +2,7 @@ function Main(){
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
         if(e.target.checkValidity()){
-            window.location.href='Home/home.html'
+            window.location.href='../Home/home.html'
         }
     });
     if (document.getElementById("input1").value === ""){
@@ -12,6 +12,6 @@ function Main(){
         document.getElementById("Error2").textContent = "Password must be at least 6 characters"
     }
     else{
-        window.location.href='Main/main.html';
+        window.location.href='../Home/home.html';
     }
 }
