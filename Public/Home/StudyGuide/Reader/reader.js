4a47ab6be96aa04355588b1bf2f0e1198bffbb21
PrinceD08 <princedavid2001101@gmail.com>
2026-01-02 01:11:42 +0100

    Added dark mode to settings

--- Public/Home/StudyGuide/Reader/reader.js
+++ Public/Home/StudyGuide/Reader/reader.js
@@ -1,4 +1,4 @@
 //GST 101
 function GST101_2() {
-    document.getElementById("topic").innerHTML = "3.2 Why Must We Listen?";
+    document.getElementById("topic").innerHTML = "";
 }
--- Public/darkMode.js
+++ Public/darkMode.js
@@ -1,3 +1,15 @@
 function darkMode(){
     document.body.style.backgroundColor = "black";
+    document.getElementById("profile").style.color = "white";
+    document.getElementById("profile2").style.color = "white";
+    document.getElementById("profile3").style.color = "white";
+    document.getElementById("profile").style.borderColor = "white";
+    document.getElementById("profile2").style.borderColor = "white";
+    document.getElementById("profile3").style.borderColor = "white";
+    document.getElementById("profile").style.boxShadow = "0px 0px 20px white";
+    document.getElementById("profile2").style.boxShadow = "0px 0px 20px white";
+    document.getElementById("profile3").style.boxShadow = "0px 0px 20px white";
+}
+function lightMode(){
+    window.location.reload();
 }
